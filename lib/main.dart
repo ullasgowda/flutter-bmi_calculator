fe7471b82e5d2ad9065c7cecebbed7d2c8ce0007
Ullas <ullasgowda42@gmail.com>
2024-01-02 19:11:05 +0530

    feat: add constants and main file

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bmi_calculator/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
+      home: const Home(),
+    );
+  }
+}
